f3f5f04ce5c94a2afe15d0d3d3100712dbb3a0d8
Sayli121 <Spusegaonkar4@gmail.com>
2022-01-26 22:22:30 +0530

    Adding new files

---
+++ Addition.java
@@ -0,0 +1,14 @@
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+int a= 10;
+int b= 98;
+int c=a+b;
+System.out.println("sum is ="+c);
+
+	}
+
+}
+
---
+++ HelloWorld1.java
@@ -0,0 +1,6 @@
+Public class HelloWorld1{
+public static void main(String[] args){
+system.out.println{"******Hello world and welcome to jenkins training***"};
+
+
+}}
