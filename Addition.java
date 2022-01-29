567dde650351b1d6defa6e00e2a4e2cd72660b77
Sayli121 <79246151+Sayli121@users.noreply.github.com>
2022-01-29 13:14:25 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -2,7 +2,7 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello world");
+System.out.println("Made some changes");
 int a= 10;
 int b= 98;
 int c=a+b;
